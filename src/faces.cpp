d947dfeea3aaf9a15989fa3406f92e31619a7f46
Jon <jon.leviathan@gmail.com>
2015-07-15 17:47:48 +0200

    cleaned cmakelists, added work in faces.cpp

--- src/faces.cpp
+++ src/faces.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "facedetector/Detection.h"
+#include <ardrone_autonomy/Navdata.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
@@ -23,6 +24,10 @@ int p_x = 0;
 int p_y = 0;
 int p_h = 0;
 int p_w = 0;
+double p_fu = 0;
+double p_fv = 0;
+double p_fd = 0;
+double p_fz = 0;
 
 int cam_h = 360;
 int cam_w = 640;
@@ -45,7 +50,7 @@ void imageCallback(const ImageConstPtr& cam_msg){
 
 }*/
 
-void callback(const facedetector::Detection::ConstPtr& det_msg, const ImageConstPtr& cam_msg){
+void callback(const facedetector::Detection::ConstPtr& det_msg, const ImageConstPtr& cam_msg, const ardrone_autonomy::Navdata::ConstPtr& nav_msg){
   // syncronised image & detection
   cv_bridge::CvImagePtr cv_ptr;
   try{
@@ -58,7 +63,7 @@ void callback(const facedetector::Detection::ConstPtr& det_msg, const ImageConst
 
   //init for temp. variables
   int x,y,w,h;
-  double fu,fv,fd;
+  double fu,fv,fd,fz;
 
   for (int i = 0; i < det_msg->image.size();i++){
     x = det_msg->x[i];y = det_msg->y[i];h = det_msg->height[i];w = det_msg->width[i];
@@ -73,14 +78,17 @@ void callback(const facedetector::Detection::ConstPtr& det_msg, const ImageConst
       fu = (x+(double)(w/2))/cam_w;
       fv = (y+(double)(h/2))/cam_h;
       fd = sqrt((cam_w*cam_h)/(w*h));
+      fz = abs(p_fu-fu)-(double)((-nav_msg->vy)/35); // 35 je FOV po y
 
-      //ROS_INFO("x = %d, y = %d, w/2 = %f, h/2 = %f", x,y,w/2,h/2);
-      ROS_INFO("fu = %f, fv = %f, fd = %f", fu, fv, fd);
-      //ROS_INFO("%d, %d", det_msg->x[i], det_msg->y[i]);
+      //ROS_INFO("fu = %f, fv = %f, fd = %f", fu, fv, fd);
+      //ROS_INFO("p_fu = %f, p_fv = %f, p_fd = %f", p_fu, p_fv, p_fd);
+      ROS_INFO("delta fu = %f, delta fv = %f, delta fd = %f", fu-p_fu, fv-p_fv, fd-p_fd);
+      //ROS_INFO("delta z = %f, delta x = %f", fz, fd-p_fd);
       ROS_INFO("\n");
 
       //save current state
       p_x = det_msg->x[i]; p_y = det_msg->y[i]; p_h = det_msg->height[i]; p_w = det_msg->width[i];
+      p_fu = fu; p_fv = fv; p_fd = fd;
     }
 
     cv::Rect r = cv::Rect(det_msg->x[i],det_msg->y[i],det_msg->height[i],det_msg->width[i]);
@@ -88,7 +96,7 @@ void callback(const facedetector::Detection::ConstPtr& det_msg, const ImageConst
   }
 
   cv:imshow(OPENCV_WINDOW, cv_ptr->image);
-  cv::waitKey(3);
+  cv::waitKey(200);
 
 }
 
@@ -104,11 +112,13 @@ int main(int argc, char **argv)
 
   message_filters::Subscriber<facedetector::Detection> sub_faces(n, "/facedetector/faces", 1);
   message_filters::Subscriber<Image> sub_camera(n, "/ardrone/image_raw", 1);
+  message_filters::Subscriber<ardrone_autonomy::Navdata> sub_navdata(n, "/ardrone/navdata",1);
+  //navdata_sub = n_.subscribe("/ardrone/navdata", 1, &MyPlugin::navdata_callback, this);
 
-  typedef sync_policies::ApproximateTime<facedetector::Detection, Image> MySyncPolicy;
+  typedef sync_policies::ApproximateTime<facedetector::Detection, Image, ardrone_autonomy::Navdata> MySyncPolicy;
 
-  Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), sub_faces, sub_camera);
-  sync.registerCallback(boost::bind(&callback, _1, _2));
+  Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), sub_faces, sub_camera, sub_navdata);
+  sync.registerCallback(boost::bind(&callback, _1, _2, _3));
 
   cv::namedWindow(OPENCV_WINDOW);
 
